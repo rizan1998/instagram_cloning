79f5390affdc92e16fe78badd077ff7333c3c1a5
rizan1998 <38775582+rizan1998@users.noreply.github.com>
2021-04-04 12:56:33 +0700

    membuat double click like pada image yang di double click

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,16 +10,17 @@
                     
                     @foreach ($posts as $post)
                     <li class="mb-3" >
-                        <img src="{{asset('images/posts/'.$post->image)}}" width="200" height="200" alt="{{$post->image}}">
+                        <img src="{{asset('images/posts/'.$post->image)}}" width="200" height="200" alt="{{$post->image}}" ondblclick="like({{$post->id}})">
                         <a href="{{'@'.$post->user->username}}">{{'@'.$post->user->username}}</a>
 
-                        <button class="btn btn-primary btn-sm"  onclick="like({{$post->id}}, this)">
+                        <button class="btn btn-primary btn-sm"  onclick="like({{$post->id}})" id="post-btn-{{$post->id}}" >
                             {{ ($post->is_liked() ? 'unlike' : 'like')}}
                         </button>
 
                     <script>
-                        function like(id, el){
-                            fetch('/like/' + id)
+                        function like(id){
+                            const el =  document.getElementById('post-btn-' + id)
+                            fetch('/like/' + id)    
                                 .then(response => response.json())
                                 // .then(data => console.log(data.status));
                                 .then(data => {
